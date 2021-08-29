f5a264b27faf09655ff34ccdb3a1e13945bdccdc
marinadumberg <marina.dumberg@inbox.lv>
2021-08-29 23:13:12 +0300

    private&public route

--- src/App.js
+++ src/App.js
@@ -3,67 +3,70 @@ import Container from "./components/Container/Container";
 import AppBar from "./components/AppBar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { Switch } from "react-router-dom";
 import { authOperations } from "./redux/auth";
-// import PrivateRoute from './components/PrivateRoute';
-// import PublicRoute from './components/PublicRoute';
-const HomeView = lazy(() =>
-  import("./views/HomeViews" /*webpackChunkName: "home-view"*/)
-);
-const RegisterView = lazy(() =>
-  import("./views/RegisterViews" /*webpackChunkName: "register-view"*/)
-);
-const LoginView = lazy(() =>
-  import("./views/LoginViews" /*webpackChunkName: "login-view"*/)
-);
-const PhonebookView = lazy(() =>
-  import("./views/PhonebookView" /*webpackChunkName: "phonebook-view"*/)
-);
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
+import authSelectors from "./redux/auth/auth-selectors";
+
+const HomeView = lazy(() => import("./views/HomeViews"));
+const RegisterView = lazy(() => import("./views/RegisterViews"));
+const LoginView = lazy(() => import("./views/LoginViews"));
+const PhonebookView = lazy(() => import("./views/PhonebookView"));
 
 export default function App() {
   const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
+
   useEffect(() => {
     dispatch(authOperations.fetchCurrentUser());
   }, [dispatch]);
 
   return (
-    <>
-      <Container>
-        <AppBar />
-        <Switch>
-          <Suspense fallback={<p>Loading...</p>}>
-            {/* <PublicRoute exact path="/"> */}
-            <HomeView />
-            {/* </PublicRoute> */}
+    !isFetchingCurrentUser && (
+      <>
+        <Container>
+          <AppBar />
+          <Switch>
+            <Suspense fallback={<p>Loading...</p>}>
+              <PublicRoute exact path="/">
+                <HomeView />
+              </PublicRoute>
 
-            {/* <PublicRoute exact path="/register" restricted> */}
-            <RegisterView />
-            {/* </PublicRoute> */}
+              <PublicRoute exact path="/register" restricted>
+                <RegisterView />
+              </PublicRoute>
 
-            {/* <PublicRoute exact path="/login" redirectTo="/phonebook" restricted> */}
-            <LoginView />
-            {/* </PublicRoute> */}
+              <PublicRoute
+                exact
+                path="/login"
+                redirectTo="/phonebook"
+                restricted
+              >
+                <LoginView />
+              </PublicRoute>
 
-            {/* <PrivateRoute path="/phonebook"> */}
-            <PhonebookView />
-            {/* </PrivateRoute> */}
-          </Suspense>
-        </Switch>
-      </Container>
+              <PrivateRoute path="/phonebook">
+                <PhonebookView />
+              </PrivateRoute>
+            </Suspense>
+          </Switch>
+        </Container>
 
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-    </>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </>
+    )
   );
 }
