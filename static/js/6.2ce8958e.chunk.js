fc7aace4a27d2ffa7b4b6dd11cf3754625b7d6e8
marinadumberg <marina.dumberg@inbox.lv>
2021-08-31 00:39:38 +0300

    Updates

---
+++ static/js/6.2ce8958e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp-marinadumberg-goit-react-hw-08-phonebook"]=this["webpackJsonp-marinadumberg-goit-react-hw-08-phonebook"]||[]).push([[6],{108:function(e,t,o){"use strict";o.r(t);o(0);var n=o(91),i=o.n(n),a=o(1);t.default=function(){return Object(a.jsx)("div",{className:i.a.container,children:Object(a.jsx)("h1",{className:i.a.title,children:"Welcome to your Phonebook"})})}},91:function(e,t,o){e.exports={container:"HomeViews_container__6YeXC",title:"HomeViews_title__1fb7k"}}}]);
+//# sourceMappingURL=6.2ce8958e.chunk.js.map
